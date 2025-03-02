ad9ef162b081c76a461691d984ca84c97df83b1c
Maniteja1098 <136944017+Maniteja1098@users.noreply.github.com>
2025-03-02 20:09:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// Firebase configuration
+       const firebaseConfig = {
+  apiKey: "AIzaSyCSA8rZtaFsjDxLe8SvU1neKtNpXOCXobY",
+  authDomain: "lensprescriptionapp-516f0.firebaseapp.com",
+  projectId: "lensprescriptionapp-516f0",
+  storageBucket: "lensprescriptionapp-516f0.firebasestorage.app",
+  messagingSenderId: "255818943089",
+  appId: "1:255818943089:web:616bf6faa4460eae3be9d0",
+  measurementId: "G-Z80F9WGLQB"
+};
+
+        // Initialize Firebase
+        const app = firebase.initializeApp(firebaseConfig);
+        const db = firebase.firestore();
+
+
 // Auto-fill the current date
         document.getElementById("currentDate").textContent = new Date().toLocaleDateString();
 
